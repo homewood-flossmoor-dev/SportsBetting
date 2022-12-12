5b91dea66eeda108f4c84fbefe90ea57388a76f3
Green-TS <117697317+Green-TS@users.noreply.github.com>
2022-12-12 15:38:02 -0600

    Update Person.py
    added init function

--- Person.py
+++ Person.py
@@ -1,11 +1,16 @@
 class User:
-    Username = str(input("Username: "))
-    Email = str(input("Email: "))
+    def __init__(self,Username,Email,Password):
+    self.Username = Username
+    self.Email = Email
+    self.Password = Password
+    
+    def Username = str(input("Username: "))
+    def Email = str(input("Email: "))
     ConfirmEmail = str(input("Confirm Email: "))
     if ConfirmEmail != Email:
         print("Emails do not match!")
     else:
-        Password = str(input("Password: "))
+        def Password = str(input("Password: "))
         ConfirmPassword = str(input("Confirm Password: "))
         if ConfirmPassword != Password:
             print("Passwords do not match!")
