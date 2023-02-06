2e5366d1f971ad33eafe37e06be76877219eae30
Green-TS <117697317+Green-TS@users.noreply.github.com>
2023-02-06 15:48:01 -0600

    Began adding encryption code

--- Person.py
+++ Person.py
@@ -1,15 +1,16 @@
 class User:
-    def __init__(self,Username,Email,Password):
+    def __init__(self,Username,Email,password):
         self.Username = Username
         self.Email = Email
-        self.Password = Password
+        self.Password = self.Encrypt(password)
     def PrintCharacteristics(self):
         print("Username: "+self.Username)
         print("Email: "+self.Email)
-
-    def Username = str(input("Username: ")) 
-
-person_one = User("Username","Email","Password")
-
-person_one.PrintCharacteristics()
-
+        print("Password: "+self.Password)
+    def Encrypt(password):
+        Encrypted_Password =""
+        for n in range(len(password)): 
+            character = ord(password[n])+3
+            Encrypted_Password+=str(character)
+        print(Encrypted_Password)
+person_one = User("Username","Email","randomkitty")
